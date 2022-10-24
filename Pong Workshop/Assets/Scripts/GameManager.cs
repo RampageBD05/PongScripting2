8085fd8af24a6b84f067bb04b722aeb66a08af4e
RampageBD05 <113905358+RampageBD05@users.noreply.github.com>
2022-10-24 14:37:24 +0200

    Added Game Manager with Restart Game and Scoring

---
+++ Pong Workshop/Assets/Scripts/GameManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    [Header("Ball Strings")]
+    public Ball ball;
+
+    [Space(20)]
+    [Header("Player Settingss")]
+    public Paddle playerPaddle;
+    public int playerScore;
+    public TextMeshProUGUI playerScoreText;
+
+    [Space(20)]
+    [Header("Computer Settings")]
+    public Paddle computerPaddle;
+    public int computerScore;
+    public TextMeshProUGUI computerScoreText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            NewGame();
+        }
+    }
+
+    public void NewGame()
+    {
+        SetPlayerScore(0);
+        SetComputerScore(0);
+        StartRound();
+    }
+
+    public void StartRound()
+    {
+        playerPaddle.ResetPosition();
+        computerPaddle.ResetPosition();
+
+        ball.ResetBall();
+        ball.AddStartForce();
+
+
+    }
+
+    public void PlayerScores()
+    {
+        SetPlayerScore(playerScore++);
+        StartRound();
+    }
+
+
+    public void ComputerScores()
+    {
+
+        SetComputerScore(computerScore++);
+        StartRound();
+    }
+  
+
+    public void SetPlayerScore(int score)
+    {
+        playerScore = score;
+        playerScoreText.text = score.ToString();
+    }
+
+
+    public void SetComputerScore(int score)
+    {
+        computerScore = score;
+        computerScoreText.text = score.ToString();
+    }
+}
